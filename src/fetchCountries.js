2de422c2bcecbcd2ef2bb2c3b7b135737487b2e1
danikCF <danik20002902@gmail.com>
2020-11-05 13:40:58 +0200

    hm 12 done

---
+++ src/fetchCountries.js
@@ -0,0 +1,50 @@
+import template from './templates.hbs'
+import listTemplate from './listCountries.hbs'
+
+import { error } from "@pnotify/core";
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+import "../node_modules/@pnotify/core/dist/PNotify.css";
+const countries = document.querySelector('.countries')
+
+let baseUrl = `https://restcountries.eu/rest/v2/name/`
+
+function createItem(temp, data, place){
+  const item = temp(data)
+  place.insertAdjacentHTML("afterbegin", item)
+}
+
+export default function fetchCountries(searchQuery){
+  countries.innerHTML = ''
+  let url = `${baseUrl}${searchQuery}`
+  return fetch(url)
+  .then(res=>{console.log(res)
+    if(res.status > 200){
+      error({
+        title: "Country not found",
+        text:
+          "Country not found",
+          delay: 2000
+      })
+    }else {
+      return res.json()
+    }
+  })
+  .then(countriesList=>{
+    console.log(countriesList)
+    if (countriesList.length > 10){
+      error({
+        title: "Attention",
+        text:
+        "Too many matches found. Please enter a more specific query!",
+        delay: 2000
+      })
+    }else if(countriesList.length >= 2 && countriesList.length <= 10){
+      createItem(listTemplate, countriesList, countries)
+    }
+    else{
+      createItem(template, countriesList, countries)
+    }
+  })
+  .catch(error =>console.log(error))
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import css from "./css/style.css";
+import fetchCountries from './fetchCountries.js'
+import debounce from 'lodash.debounce';
+const input = document.getElementById("searchInput");
+input.addEventListener('input', debounce((e)=>{
+  fetchCountries(e.target.value);
+  input.value = '';
+},700));
